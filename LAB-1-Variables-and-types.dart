edca627c54811b9360d6890c5c4ce59cb2b50b9e
Abdulazez <abzaek@hotmail.com>
2024-03-08 14:46:30 +0300

    Add Dart code with string interpolation

---
+++ LAB-1-Variables-and-types.dart
@@ -0,0 +1,7 @@
+void main() {
+  String name = 'Abdulazez Zeinu';
+  int age = 21;
+  String favoriteColor = 'Black';
+
+  print("My name is $name. I am $age years old. My favorite color is $favoriteColor");
+}
