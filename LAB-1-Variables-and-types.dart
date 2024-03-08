7f42577441add190a5dceff9e01fc1bd8dea203c
Abdulazez <abzaek@hotmail.com>
2024-03-08 15:19:35 +0300

    Add code demonstrating Variables and Types in Dart

--- LAB-1-Variables-and-types.dart
+++ LAB-1-Variables-and-types.dart
@@ -1,7 +1,24 @@
+import "dart:io";
+import "dart:async";
+
 void main() {
+  // Exercise 1
   String name = 'Abdulazez Zeinu';
   int age = 21;
   String favoriteColor = 'Black';
 
   print("My name is $name. I am $age years old. My favorite color is $favoriteColor");
+
+  // Exercise 2
+  const speedOfLight = 299792458; 
+  
+  print("Enter the distance light needs to travel (in meters):");
+  
+  try {
+    double distance = double.parse(stdin.readLineSync()!);
+    double timeInSeconds = distance / speedOfLight;
+    print("Time taken for light to travel $distance meters: $timeInSeconds seconds");
+  } catch (e) {
+    print("Invalid input. Please enter a valid number.");
+  }
 }
