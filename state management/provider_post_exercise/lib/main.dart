5b6136d498e9d25a6362eabd6d072b40c8843c1a
Abdulazez <abzaek@hotmail.com>
2024-04-21 23:11:17 +0300

    added code to provider post state management

---
+++ state management/provider_post_exercise/lib/main.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => PostProvider(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const PostListScreen(),
+      ),
+    );
+  }
+}
+
+class PostModel {
+  final int id;
+  final int userId;
+  final String title;
+  final String body;
+
+  PostModel({
+    required this.id,
+    required this.userId,
+    required this.title,
+    required this.body,
+  });
+
+  factory PostModel.fromJson(Map<String, dynamic> json) {
+    return PostModel(
+      id: json['id'],
+      userId: json['userId'],
+      title: json['title'],
+      body: json['body'],
+    );
+  }
+}
+
+class PostService {
+  Future<List<PostModel>> getPosts() async {
+    final response =
+        await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+    if (response.statusCode == 200) {
+      List<dynamic> jsonList = json.decode(response.body);
+      return jsonList.map((json) => PostModel.fromJson(json)).toList();
+    } else {
+      throw Exception('Failed to load posts');
+    }
+  }
+}
+
+class PostProvider extends ChangeNotifier {
+  List<PostModel> _posts = [];
+  final PostService _postService = PostService();
+
+  List<PostModel> get posts => _posts;
+
+  Future<void> fetchPosts() async {
+    try {
+      _posts = await _postService.getPosts();
+      notifyListeners();
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+}
+
+class PostListScreen extends StatefulWidget {
+  const PostListScreen({super.key});
+
+  @override
+  _PostListScreenState createState() => _PostListScreenState();
+}
+
+class _PostListScreenState extends State<PostListScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration.zero, () {
+      Provider.of<PostProvider>(context, listen: false).fetchPosts();
+    }).then((value) => setState(() {}));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final postProvider = Provider.of<PostProvider>(context);
+    final posts = postProvider.posts;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Posts'),
+      ),
+      body: posts.isEmpty
+          ? const Center(
+              child: Text(
+                'L O A D I N G . . .',
+                style: TextStyle(fontSize: 20),
+              ),
+            )
+          : ListView.builder(
+              itemCount: posts.length,
+              itemBuilder: (context, index) {
+                final post = posts[index];
+                return Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.blueAccent),
+                    borderRadius: BorderRadius.circular(10.0),
+                    color: Colors.grey.shade200,
+                  ),
+                  margin: const EdgeInsets.all(8.0),
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        post.title,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18.0,
+                        ),
+                      ),
+                      const SizedBox(height: 8.0),
+                      Text(
+                        post.body,
+                        style: const TextStyle(fontSize: 16.0),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+    );
+  }
+}
