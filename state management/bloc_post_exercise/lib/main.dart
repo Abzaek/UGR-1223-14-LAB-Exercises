dc3b819ddb76269392805e9e44dcec4cadb02926
Abdulazez <abzaek@hotmail.com>
2024-04-21 23:37:01 +0300

    added code to bloc post exercise

---
+++ state management/bloc_post_exercise/lib/main.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Post Viewer',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: BlocProvider(
+        create: (context) => PostBloc(PostService()),
+        child: const PostListScreen(),
+      ),
+    );
+  }
+}
+
+class Post {
+  final int id;
+  final int userId;
+  final String title;
+  final String body;
+
+  Post({
+    required this.id,
+    required this.userId,
+    required this.title,
+    required this.body,
+  });
+
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(
+      id: json['id'],
+      userId: json['userId'],
+      title: json['title'],
+      body: json['body'],
+    );
+  }
+}
+
+class PostService {
+  Future<List<Post>> fetchPosts() async {
+    final response =
+        await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+    if (response.statusCode == 200) {
+      List<dynamic> jsonList = json.decode(response.body);
+      return jsonList.map((json) => Post.fromJson(json)).toList();
+    } else {
+      throw Exception('Failed to load posts');
+    }
+  }
+}
+
+class PostBloc extends Cubit<List<Post>> {
+  final PostService postService;
+
+  PostBloc(this.postService) : super([]);
+
+  void fetchPosts() async {
+    try {
+      final posts = await postService.fetchPosts();
+      emit(posts);
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+}
+
+class PostListScreen extends StatelessWidget {
+  const PostListScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    final postBloc = BlocProvider.of<PostBloc>(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Post Viewer'),
+      ),
+      body: BlocBuilder<PostBloc, List<Post>>(
+        builder: (context, posts) {
+          if (posts.isEmpty) {
+            postBloc.fetchPosts(); // Fetch posts when the list is empty
+            return const Center(
+              child: Text(
+                'L O A D I N G . . .',
+                style: TextStyle(fontSize: 20),
+              ),
+            );
+          } else {
+            return ListView.builder(
+              itemCount: posts.length,
+              itemBuilder: (context, index) {
+                final post = posts[index];
+                return Container(
+                  margin: const EdgeInsets.symmetric(
+                      vertical: 8.0, horizontal: 16.0),
+                  padding: const EdgeInsets.all(12.0),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        post.title,
+                        style: const TextStyle(
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 8.0),
+                      Text(
+                        post.body,
+                        style: const TextStyle(
+                          fontSize: 16.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            );
+          }
+        },
+      ),
+    );
+  }
+}
